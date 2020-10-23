dd11128a860af1fde27e9b286ae1655088c724d2
ted <tedmarov@gmail.com>
2020-10-23 14:48:22 -0500

    Ride and Food works

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,8 @@
 // Import and invoke the ticket booth component function
+import { FoodTicketHolder } from "./rides/FoodTicketHolder.js"
+import { RideTicketHolders } from "./rides/RideTicketHolders.js"
 import { TicketBooth } from "./TicketBooth.js"
 
 TicketBooth()
+RideTicketHolders()
+FoodTicketHolder()
